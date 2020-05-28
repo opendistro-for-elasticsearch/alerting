6f9a1edb6d6ac4bc3244b58f7a43fa0b08ee97d7
Tianli Feng <ftianli@amazon.com>
2020-05-28 11:01:13 -0700

    change default value of the setting 'alert_history_max_age' to 30 days

--- alerting/src/main/kotlin/com/amazon/opendistroforelasticsearch/alerting/settings/AlertingSettings.kt
+++ alerting/src/main/kotlin/com/amazon/opendistroforelasticsearch/alerting/settings/AlertingSettings.kt
@@ -91,7 +91,7 @@ class AlertingSettings {
 
         val ALERT_HISTORY_INDEX_MAX_AGE = Setting.positiveTimeSetting(
                 "opendistro.alerting.alert_history_max_age",
-                TimeValue.timeValueHours(24),
+                TimeValue(30, TimeUnit.DAYS),
                 Setting.Property.NodeScope, Setting.Property.Dynamic
         )
 
