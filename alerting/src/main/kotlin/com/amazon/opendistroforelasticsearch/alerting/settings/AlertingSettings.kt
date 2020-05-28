a46c3c29b7391bbc0b1995222664af303c3b3207
Tianli Feng <ftianli@amazon.com>
2020-05-28 16:20:12 -0700

    Change default value of the index rollover condition (#206)
    * change default value of the setting 'alert_history_max_age' to 30 days
    
    * change default value of the setting 'alert_history_retention_period' to 60 days

--- alerting/src/main/kotlin/com/amazon/opendistroforelasticsearch/alerting/settings/AlertingSettings.kt
+++ alerting/src/main/kotlin/com/amazon/opendistroforelasticsearch/alerting/settings/AlertingSettings.kt
@@ -91,7 +91,7 @@ class AlertingSettings {
 
         val ALERT_HISTORY_INDEX_MAX_AGE = Setting.positiveTimeSetting(
                 "opendistro.alerting.alert_history_max_age",
-                TimeValue.timeValueHours(24),
+                TimeValue(30, TimeUnit.DAYS),
                 Setting.Property.NodeScope, Setting.Property.Dynamic
         )
 
@@ -104,7 +104,7 @@ class AlertingSettings {
 
         val ALERT_HISTORY_RETENTION_PERIOD = Setting.positiveTimeSetting(
                 "opendistro.alerting.alert_history_retention_period",
-                TimeValue(30, TimeUnit.DAYS),
+                TimeValue(60, TimeUnit.DAYS),
                 Setting.Property.NodeScope, Setting.Property.Dynamic
         )
 
