b70e1926580a858446051efaf7fa562ce533dfc3
Mohammad Qureshi <qreshi@amazon.com>
2020-08-30 22:57:34 -0700

    Move email_accounts and email_groups APIs under 'destinations' namespace

--- alerting/src/main/kotlin/com/amazon/opendistroforelasticsearch/alerting/AlertingPlugin.kt
+++ alerting/src/main/kotlin/com/amazon/opendistroforelasticsearch/alerting/AlertingPlugin.kt
@@ -109,8 +109,8 @@ internal class AlertingPlugin : PainlessExtension, ActionPlugin, ScriptPlugin, R
         @JvmField val UI_METADATA_EXCLUDE = arrayOf("monitor.${Monitor.UI_METADATA_FIELD}")
         @JvmField val MONITOR_BASE_URI = "/_opendistro/_alerting/monitors"
         @JvmField val DESTINATION_BASE_URI = "/_opendistro/_alerting/destinations"
-        @JvmField val EMAIL_ACCOUNT_BASE_URI = "/_opendistro/_alerting/email_accounts"
-        @JvmField val EMAIL_GROUP_BASE_URI = "/_opendistro/_alerting/email_groups"
+        @JvmField val EMAIL_ACCOUNT_BASE_URI = "$DESTINATION_BASE_URI/email_accounts"
+        @JvmField val EMAIL_GROUP_BASE_URI = "$DESTINATION_BASE_URI/email_groups"
         @JvmField val ALERTING_JOB_TYPES = listOf("monitor")
     }
 
