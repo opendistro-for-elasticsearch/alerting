2451e212e40ab4a0305b5ad40f28697cf13f45d8
Ashish Agrawal <ashisagr@amazon.com>
2020-12-02 10:04:08 -0800

    Support filterBy in update/delete destination/monitor APIs (#311)
    * Support filterBy in update/delete destination/monitor APIs
    
    * cleanup code
    
    * fix message
    
    * add comments to checkUserFilterByPermissions function

--- alerting/src/test/kotlin/com/amazon/opendistroforelasticsearch/alerting/resthandler/SecureDestinationRestApiIT.kt
+++ alerting/src/test/kotlin/com/amazon/opendistroforelasticsearch/alerting/resthandler/SecureDestinationRestApiIT.kt
@@ -86,6 +86,130 @@ class SecureDestinationRestApiIT : AlertingRestTestCase() {
         }
     }
 
+    fun `test update destination with disable filter by`() {
+        disableFilterBy()
+
+        val chime = Chime("http://abc.com")
+        val destination = Destination(
+            type = DestinationType.CHIME,
+            name = "test",
+            user = randomUser(),
+            lastUpdateTime = Instant.now(),
+            chime = chime,
+            slack = null,
+            customWebhook = null,
+            email = null)
+        val createdDestination = createDestination(destination = destination)
+
+        assertEquals("Incorrect destination name", createdDestination.name, "test")
+        assertEquals("Incorrect destination type", createdDestination.type, DestinationType.CHIME)
+
+        val slack = Slack("http://abc.com")
+        val destinationV2 = createdDestination.copy(name = "testUpdate", type = DestinationType.SLACK, chime = null, slack = slack)
+
+        val updatedDestination = updateDestination(destination = destinationV2)
+        assertEquals("Incorrect destination name", updatedDestination.name, "testUpdate")
+        assertEquals("Incorrect destination type", updatedDestination.type, DestinationType.SLACK)
+    }
+
+    fun `test update destination with enable filter by`() {
+        enableFilterBy()
+        if (!isHttps()) {
+            // if security is disabled and filter by is enabled, we can't create monitor
+            // refer: `test create destination with enable filter by`
+            return
+        }
+
+        val chime = Chime("http://abc.com")
+        val destination = Destination(
+            type = DestinationType.CHIME,
+            name = "test",
+            user = randomUser(),
+            lastUpdateTime = Instant.now(),
+            chime = chime,
+            slack = null,
+            customWebhook = null,
+            email = null)
+
+        // 1. create a destination as admin user
+        val createdDestination = createDestination(destination, true)
+
+        assertEquals("Incorrect destination name", createdDestination.name, "test")
+        assertEquals("Incorrect destination type", createdDestination.type, DestinationType.CHIME)
+
+        val slack = Slack("http://abc.com")
+        val destinationV2 = createdDestination.copy(name = "testUpdate", type = DestinationType.SLACK, chime = null, slack = slack)
+
+        val updatedDestination = updateDestination(destination = destinationV2)
+        assertEquals("Incorrect destination name", updatedDestination.name, "testUpdate")
+        assertEquals("Incorrect destination type", updatedDestination.type, DestinationType.SLACK)
+    }
+
+    fun `test delete destination with disable filter by`() {
+        disableFilterBy()
+
+        val chime = Chime("http://abc.com")
+        val destination = Destination(
+            type = DestinationType.CHIME,
+            name = "test",
+            user = randomUser(),
+            lastUpdateTime = Instant.now(),
+            chime = chime,
+            slack = null,
+            customWebhook = null,
+            email = null)
+        val createdDestination = createDestination(destination = destination)
+
+        assertEquals("Incorrect destination name", createdDestination.name, "test")
+        assertEquals("Incorrect destination type", createdDestination.type, DestinationType.CHIME)
+
+        deleteDestination(createdDestination)
+
+        val inputMap = HashMap<String, Any>()
+        inputMap["missing"] = "_last"
+        inputMap["destinationType"] = "chime"
+
+        // get destinations as admin user
+        val adminResponse = getDestinations(client(), inputMap)
+        assertEquals(0, adminResponse.size)
+    }
+
+    fun `test delete destination with enable filter by`() {
+        enableFilterBy()
+        if (!isHttps()) {
+            // if security is disabled and filter by is enabled, we can't create monitor
+            // refer: `test create destination with enable filter by`
+            return
+        }
+
+        val chime = Chime("http://abc.com")
+        val destination = Destination(
+            type = DestinationType.CHIME,
+            name = "test",
+            user = randomUser(),
+            lastUpdateTime = Instant.now(),
+            chime = chime,
+            slack = null,
+            customWebhook = null,
+            email = null)
+
+        // 1. create a destination as admin user
+        val createdDestination = createDestination(destination, true)
+
+        assertEquals("Incorrect destination name", createdDestination.name, "test")
+        assertEquals("Incorrect destination type", createdDestination.type, DestinationType.CHIME)
+
+        deleteDestination(createdDestination)
+
+        val inputMap = HashMap<String, Any>()
+        inputMap["missing"] = "_last"
+        inputMap["destinationType"] = "chime"
+
+        // 2. get destinations as admin user
+        val adminResponse = getDestinations(client(), inputMap)
+        assertEquals(0, adminResponse.size)
+    }
+
     fun `test get destinations with a destination type and disable filter by`() {
         disableFilterBy()
         val slack = Slack("url")
